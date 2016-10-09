4daafd85b567f8249cea2cf41db737613d727872
Ryan Riddle <rcri222@gmail.com>
2016-10-08 21:57:50 -0400

    implemented finger tables for nodes.  nodes can use their finger tables to find values

--- chord.rb
+++ chord.rb
@@ -43,57 +43,125 @@ class OpenClosedInterval < Interval
   end
 end
 
+class OpenOpenInterval < Interval
+  def contains? (val)
+    d1 = difference(@first, val)
+    d2 = difference(@last, val)
+
+    0 < d1 and 0 < d2 and d1 < d2
+  end
+end
+
+class FingerEntry
+  attr_reader :start, :interval, :node
+  
+  def initialize(start, finish, node)
+    @start = start
+    @interval = ClosedOpenInterval.new(start, finish)
+    @node = node
+  end
+end
+
 class Node
 
   attr_reader :id
-  attr_reader :predecessor
-  attr_reader :successor
-  attr_reader :data
 
   def initialize(id)
     @id = id
     @data = {}
+    @finger = []
   end
 
-=begin
-  def diff(key)
-    (@id - key) % KEYSPACE_SIZE
-  end
-=end
-
-  def successor=(n)
-    @successor = n
+  def successor
+    if @finger.empty?
+      nil
+    else
+      @finger[0].node
+    end
   end
 
   def predecessor=(n)
     @predecessor = n
   end
 
+  def add_finger(f)
+    @finger.push(f)
+  end
+
+  def find_successor(key)
+    n = find_predecessor(key)
+    n.successor
+  end
+
+  def find_predecessor(key)
+    n = self
+    r = OpenClosedInterval.new(n.id, n.successor.id)
+
+    while not r.contains? key
+      n = n.closest_preceding_finger(key)
+      r = OpenClosedInterval.new(n.id, n.successor.id)
+    end
+
+    n
+  end
+
+  def closest_preceding_finger(key)
+    r = OpenOpenInterval.new(@id, key)
+    @finger.reverse_each do |f|
+      if (r.contains? f.node.id)
+        return f.node
+      end
+    end
+
+    return self
+  end
+      
+
   def owns?(key)
-    if predecessor.nil?
+    if @predecessor.nil?
       return true
     end
 
     r = OpenClosedInterval.new(@predecessor.id, @id)
     r.contains? key
-
-    #return diff(key) < @predecessor.diff(key)
   end
 
   def store(key, value)
     if owns? key
-      data[key] = value
+      @data[key] = value
     else
-      @successor.store(key, value)
+      successor.store(key, value)
     end
   end
 
   def get(key)
     if owns? key
-      data[key]
+      @data[key]
     else
-      @successor.get(key)
+      n = find_successor(key)
+      n.get(key)
     end
   end
   
 end
+
+
+a = Node.new 0
+b = Node.new 1
+c = Node.new 3
+
+a.predecessor = c
+b.predecessor = a
+c.predecessor = b
+
+a.add_finger FingerEntry.new(1, 2, b)
+a.add_finger FingerEntry.new(2, 4, c)
+a.add_finger FingerEntry.new(4, 0, a)
+
+b.add_finger FingerEntry.new(2, 3, c)
+b.add_finger FingerEntry.new(3, 5, c)
+b.add_finger FingerEntry.new(5, 1, a)
+
+c.add_finger FingerEntry.new(4, 5, a)
+c.add_finger FingerEntry.new(5, 7, a)
+c.add_finger FingerEntry.new(7, 3, a)
