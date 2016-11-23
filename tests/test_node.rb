36684275344932421c428ba1f7556c03fa518c77
Ryan Riddle <rcri222@gmail.com>
2016-11-23 10:49:10 -0500

    started to update the tests file

--- tests/test_node.rb
+++ tests/test_node.rb
@@ -2,9 +2,8 @@ require 'test/unit'
 require 'chord'
 
 class NodeTests < Test::Unit::TestCase
-  def test_finger
-    a = Node.new 0
-
+	def test_finger
+		a = Node.new 'localhost', 50000
     assert(a.finger.length == M)
   end
 end
