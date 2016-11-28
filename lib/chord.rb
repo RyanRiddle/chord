e593fd777dcb7802a40a827b08ac050506bb4649
Ryan Riddle <rcri222@gmail.com>
2016-11-27 19:01:33 -0500

    sleeping during key transfer was a dumb idea.  it means you node pauses for 60 seconds when it firsts connects to the network

--- lib/chord.rb
+++ lib/chord.rb
@@ -252,7 +252,6 @@ class Node
   end
 
   def transfer_keys(n)
-		sleep 60		# sleep to make sure we are not transfering too much during initial setup
 		n = @predecessor
 		if n.addr != @addr
 			puts "transferring..."
